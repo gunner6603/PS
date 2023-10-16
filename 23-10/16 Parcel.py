608e8309192f5031f64d810c8cb750e99546a0fc
Geon <gunner6603@snu.ac.kr>
2023-10-16 15:59:13 +0900

    231016 update

--- 23-10/16 Parcel.py
+++ 23-10/16 Parcel.py
@@ -7,22 +7,18 @@ inp = sys.stdin.readline
 W, N = map(int, inp().split())
 
 elems = list(map(int, inp().split()))
-dp = [None] * W
+dp = [False] * W
 
 for i in range(N):
+    ei = elems[i]
     for j in range(i + 1, N):
-        s = elems[i] + elems[j]
+        s = ei + elems[j]
+        if s < W and dp[W - s]:
+            print("YES")
+            exit()
+    for j in range(i):
+        s = ei + elems[j]
         if s < W:
-            dp[s] = (i, j)
-
-for i in range(N):
-    for j in range(i + 1, N):
-        s = elems[i] + elems[j]
-        if s < W:
-            val = dp[W - s]
-            if s < W and val:
-                if i not in val and j not in val:
-                    print("YES")
-                    exit()
-
+            dp[s] = True
+            
 print("NO")
